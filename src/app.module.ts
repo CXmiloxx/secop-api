b11941e41edc6fcd9a5d2d1c5566bcfa0bb6aa31
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2026-01-04 23:37:54 -0500

    ✨feat: añade modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { RolesGuard } from '@/common/guards/roles.guard';
 import { PrismaModule } from '@prisma/prisma.module';
 import { ProviderModule } from '@/provider/provider.module';
 import { JwtAuthGuard } from '@/common/guards/jwt-auth.guard';
+import { CuentasContablesModule } from './cuentas-contables/cuentas-contables.module';
+import { ConceptosModule } from './conceptos/conceptos.module';
+import { PresupuestoModule } from './presupuesto/presupuesto.module';
+import { ProductosModule } from './productos/productos.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ProviderModule],
+  imports: [PrismaModule, AuthModule, ProviderModule, CuentasContablesModule, ConceptosModule, PresupuestoModule, ProductosModule],
   controllers: [],
   providers: [
     {
