74d75b417fd83621a119b1c1225688a0ed697e6f
Camilo Finova <juancamilog9911@gmail.com>
2026-01-06 15:14:05 +0000

    🔥remove : elimina modulo de presupuesto

--- src/presupuesto/presupuesto.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PresupuestoService } from './presupuesto.service';
-import { PresupuestoController } from './presupuesto.controller';
-
-@Module({
-  controllers: [PresupuestoController],
-  providers: [PresupuestoService],
-})
-export class PresupuestoModule {}
